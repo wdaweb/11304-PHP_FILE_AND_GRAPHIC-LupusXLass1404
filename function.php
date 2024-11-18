54c0fef6842650cf657f6dc6c2e05f96ccdce3a5
LupusXLass1404 <berkasxlupusxlass@gmail.com>
2024-11-18 10:12:38 +0800

    上傳檔案

---
+++ function.php
@@ -0,0 +1,10 @@
+<?php
+
+function dd($ele){
+    echo "<pre>";
+    print_r($ele);
+    echo "</pre>";
+}
+
+
+?>
--- upload.php
+++ upload.php
@@ -9,6 +9,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,16 +17,22 @@
     <title>檔案上傳</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
- <h1 class="header">檔案上傳練習</h1>
- <!----建立你的表單及設定編碼----->
+
+<body class="unload">
+    <h1 class="header">檔案上傳練習</h1>
+    <!----建立你的表單及設定編碼----->
+    <form action="manage.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="img" id="file" enctype="multipart/form-date">
+        <input type="text" name="name" id="name">
+        <input type="submit" value="上傳">
+    </form>
 
 
 
 
-
-<!----建立一個連結來查看上傳後的圖檔---->  
+    <!----建立一個連結來查看上傳後的圖檔---->
 
 
 </body>
+
 </html>
